567ba097200bc03432bb478ecc8da9bcdff984c3
robot691 <nhinkle4945@student.fortosage.net>
2025-03-03 09:55:40 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+# Install easyocr
+pip install easyocr
+
+# Download main.py
+wget https://raw.githubusercontent.com/robot691/PythonExtractor/refs/heads/main/main.py
+
+echo "Installation and download complete."
